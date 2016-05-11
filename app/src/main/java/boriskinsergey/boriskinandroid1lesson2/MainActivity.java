17838cb7006d93e9efa9eae1eb01d431796d3b34
Sergey <boriskin.serge@gmail.com>
2016-05-11 20:46:37 +0300

    Добавлена обработка массива навыков

--- app/src/main/java/boriskinsergey/boriskinandroid1lesson2/MainActivity.java
+++ app/src/main/java/boriskinsergey/boriskinandroid1lesson2/MainActivity.java
@@ -14,13 +14,26 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Resources res;
     TextView txt2;
+    TextView txtSkills=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         res=getResources();
         txt2=(TextView) findViewById(R.id.txt2);
-        txt2.setText(res.getString(R.string.name_f)+" "+res.getString(R.string.name_n)+" "+res.getString(R.string.name_s));
 
+
+        txt2.setText(res.getString(R.string.name_f)+" "+res.getString(R.string.name_n)+" "+res.getString(R.string.name_s));
+        String[] skillsArray = res.getStringArray(R.array.skills);
+
+        StringBuilder skilsSB = new StringBuilder();
+        skilsSB.append("Навыки в сфере IT: \n");
+
+        for (int i = 0; i < skillsArray.length; i++) {
+            skilsSB.append(skillsArray[i]+"\n");
+        }
+        txtSkills=(TextView) findViewById(R.id.txtSkills);
+        txtSkills.setText(skilsSB.toString());
     }
 }
